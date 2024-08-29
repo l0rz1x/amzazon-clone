64947d8c71c4ad3943aecef8ae66aa4304c3b00a
l0rz1x <usamehariri22@gmail.com>
2024-08-29 15:02:15 +0300

    remove default tests and add money tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,13 @@
+import { calmoney } from '../scripts/utils/money.js';
+
+describe('case suit: formatCurrency', () => {
+    it('convert cents into dollar', () => {
+        expect(calmoney(2095)).toEqual('20.95');
+    });
+    it('Works with 0', () => {
+        expect(calmoney(0)).toEqual('0.00');
+    });
+    it('Rounds up to nearest cent', () => {
+        expect(calmoney(2000.5)).toEqual('20.01');
+    });
+});
